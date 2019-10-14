a562669df07f9bd02df2dafd7d7da2496401e890
KnightShade <saurao2612@gmail.com>
2019-10-14 19:11:18 +0530

    shows some output

--- NB-Spam-Filter/main.py
+++ NB-Spam-Filter/main.py
@@ -15,16 +15,15 @@ f = open("./newSpamDataSet.csv")
 allSentencesListWithLabel = []
 
 for line in f:
-    sentenceList = preprocess(line.split(",", 1)[1])
-    allSentencesListWithLabel.append([line.split(",")[0], sentenceList])
+	allSentencesListWithLabel.append([line.split(",")[0], line.split(",", 1)[1]])
 
 allSentencesListWithLabel.pop(0)
 
 shuffle(allSentencesListWithLabel)
 
-allSentencesList = [sentence[1] for sentence in allSentencesListWithLabel[:1600]]
-spamSentencesList = [sentence[1] for sentence in allSentencesListWithLabel[:1600] if (sentence[0] == "spam")]
-hamSentencesList = [sentence[1] for sentence in allSentencesListWithLabel[:1600] if (sentence[0] == "ham")]
+allSentencesList = [preprocess(sentence[1]) for sentence in allSentencesListWithLabel[:1600]]
+spamSentencesList = [preprocess(sentence[1]) for sentence in allSentencesListWithLabel[:1600] if (sentence[0] == "spam")]
+hamSentencesList = [preprocess(sentence[1]) for sentence in allSentencesListWithLabel[:1600] if (sentence[0] == "ham")]
 testSentencesList = allSentencesListWithLabel[1600:]
 
 # print(allSentencesList, spamSentencesList, hamSentencesList)
@@ -34,19 +33,30 @@ bowAll = bagOfWords(allSentencesList)
 bowSpam = bagOfWords(spamSentencesList)
 bowHam = bagOfWords(hamSentencesList)
 
-print(colorama.Fore.LIGHTBLUE_EX)
-
 # print(bowAll, bowSpam, bowHam, sep = "\n")
 
 count = 0
+spamCount = 0
+hamCount = 0
 
 for testList in testSentencesList:
-    res = naiveBayes(testList[1], bowAll, bowSpam, bowHam)
+    res = naiveBayes(preprocess(testList[1]), bowAll, bowSpam, bowHam)
     # print(testList[0], res)
     if(res == testList[0]):
+        if(((res == "ham") and (hamCount < 2)) or ((res == "spam") and (spamCount < 2))):
+            print("Input:", testList[1], end = "")
+            print("Expected output:", testList[0])
+            print("Predicted output:", res)
+            print()
+            if(res == "ham"): 
+                hamCount += 1
+            else: 
+                spamCount += 1
         count += 1
 
 programEnd = time()
+
+print(colorama.Fore.LIGHTBLUE_EX)
 
 table.add_row(["Total data", len(allSentencesListWithLabel)])
 table.add_row(["Total train data", (len(spamSentencesList) + len(hamSentencesList))])
